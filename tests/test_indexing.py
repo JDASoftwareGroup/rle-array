20afb3dd02d2128fc1e0dda357e9aeedcd0da944
Marco Neumann <marco.neumann@jda.com>
2020-02-04 14:45:27 +0100

    add missing test

---
+++ tests/test_indexing.py
@@ -0,0 +1,12 @@
+import pytest
+
+from rle_array import RLEArray
+
+
+def test_fail_two_dim_indexing():
+    array = RLEArray._from_sequence(range(10))
+    with pytest.raises(
+        NotImplementedError,
+        match="__getitem__ does currently only work w/ a single parameter",
+    ):
+        array[1, 2]
