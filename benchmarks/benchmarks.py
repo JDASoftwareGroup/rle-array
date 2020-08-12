64ef71dceefa1d87de5c0c51d061ecf97a6c5a90
Marco Neumann <marco.neumann@blueyonder.com>
2020-08-12 09:29:39 +0200

    cleanup benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,6 +1,8 @@
 import warnings
 from contextlib import contextmanager
+from typing import Generator
 
+import pandas as pd
 from pandas.errors import PerformanceWarning
 
 from rle_array.testing import const_col, dim_col, generate_test_dataframe
@@ -13,22 +15,22 @@ class Base:
     sample_time = 1.0
     warmup_time = 1.0
 
-    def gen_baseline(self):
+    def gen_baseline(self) -> pd.DataFrame:
         return generate_test_dataframe(n_dims=3, size=100)
 
-    def setup(self):
+    def setup(self) -> None:
         self.df_baseline = self.gen_baseline()
         self.df_rle = self.df_baseline.astype("RLEDtype[int64]")
 
     @contextmanager
-    def ignore_performance_warnings(self):
+    def ignore_performance_warnings(self) -> Generator[None, None, None]:
         with warnings.catch_warnings():
             warnings.simplefilter(action="ignore", category=PerformanceWarning)
             yield
 
 
-class TimeAccess(Base):
-    def setup(self):
+class TimeTake(Base):
+    def setup(self) -> None:
         super().setup()
 
         self.shuffle_dim2_unstable = self.df_baseline.sort_values(
@@ -38,21 +40,21 @@ class TimeAccess(Base):
             dim_col(2), kind="mergesort"
         ).index.values
 
-    def time_take_unstable_const12_base(self):
+    def time_unstable_const12_base(self) -> None:
         self.df_baseline[const_col([1, 2])].take(self.shuffle_dim2_unstable)
 
-    def time_take_unstable_const12_rle(self):
+    def time_unstable_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])].take(self.shuffle_dim2_unstable)
 
-    def time_take_stable_const12_base(self):
+    def time_stable_const12_base(self) -> None:
         self.df_baseline[const_col([1, 2])].take(self.shuffle_dim2_stable)
 
-    def time_take_stable_const12_rle(self):
+    def time_stable_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])].take(self.shuffle_dim2_stable)
 
 
 class TimeGroupByReduce(Base):
-    def setup(self):
+    def setup(self) -> None:
         super().setup()
 
         df_rle_wo_dims = self.df_rle.copy()
@@ -60,56 +62,56 @@ class TimeGroupByReduce(Base):
             df_rle_wo_dims[dim_col(d)] = self.df_baseline[dim_col(d)].copy()
         self.df_rle_wo_dims = df_rle_wo_dims
 
-    def time_groupby2_sum_const12_baseline(self):
+    def time_key2_opsum_const12_baseline(self) -> None:
         self.df_baseline.groupby(dim_col(2))[const_col([1, 2])].sum()
 
-    def time_groupby2_sum_const12_rle(self):
+    def time_key2_opsum_const12_rle(self) -> None:
         with self.ignore_performance_warnings():
             self.df_rle_wo_dims.groupby(dim_col(2))[const_col([1, 2])].sum()
 
 
 class TimeSeriesReduce(Base):
-    def time_sum_const12_baseline(self):
+    def time_sum_const12_baseline(self) -> None:
         self.df_baseline[const_col([1, 2])].sum()
 
-    def time_sum_const12_rle(self):
+    def time_sum_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])].sum()
 
-    def time_sum_const012_baseline(self):
+    def time_sum_const012_baseline(self) -> None:
         self.df_baseline[const_col([0, 1, 2])].sum()
 
-    def time_sum_const012_rle(self):
+    def time_sum_const012_rle(self) -> None:
         self.df_rle[const_col([0, 1, 2])].sum()
 
 
 class TimeShift(Base):
-    def time_shift_int_const12_base(self):
+    def time_int_const12_base(self) -> None:
         self.df_baseline[const_col([1, 2])].shift(periods=1, fill_value=1)
 
-    def time_shift_int_const12_rle(self):
+    def time_int_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])].shift(periods=1, fill_value=1)
 
-    def time_shift_float_const12_base(self):
+    def time_float_const12_base(self) -> None:
         self.df_baseline[const_col([1, 2])].shift(periods=1)
 
-    def time_shift_float_const12_rle(self):
+    def time_float_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])].shift(periods=1)
 
 
 class TimeOperator(Base):
-    def time_add_const12_baseline(self):
+    def time_add_const12_baseline(self) -> None:
         self.df_baseline[const_col([1, 2])] + self.df_baseline[const_col([1, 2])]
 
-    def time_add_const12_rle(self):
+    def time_add_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])] + self.df_rle[const_col([1, 2])]
 
-    def time_eq_const12_baseline(self):
+    def time_eq_const12_baseline(self) -> None:
         self.df_baseline[const_col([1, 2])] == self.df_baseline[const_col([1, 2])]
 
-    def time_eq_const12_rle(self):
+    def time_eq_const12_rle(self) -> None:
         self.df_rle[const_col([1, 2])] == self.df_rle[const_col([1, 2])]
 
 
 class TimeGenerateTestDataFrame(Base):
-    def time_generate_test_dataframe(self):
+    def time(self) -> None:
         self.gen_baseline()
